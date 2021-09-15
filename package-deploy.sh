9d32bb01816e70841662c1b9aef076fa9ae033f4
rk365040 <rknaidu08@gmail.com>
2021-09-15 22:01:20 +0530

    Update package-deploy.sh

--- package-deploy.sh
+++ package-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "downloading war file"
 
-wget http://3.84.44.206:8081/nexus/service/local/repositories/maven/content/java/sparkjava-hello-world/$version/sparkjava-hello-world-$version.war
+wget --user=ram --password=$1 http://3.84.44.206:8081/nexus/service/local/repositories/maven/content/java/sparkjava-hello-world/$version/sparkjava-hello-world-$version.war
